0fdb8eeaf973153c2a436c7a13faf149c6bbb05e
akshay dhangar <akshaygdhangar14@gmail.com>
2022-07-11 13:00:42 +0530

    UC2_FileOperations

---
+++ UC_2/UC2_DemonstrateFileOperations/EmployeePayrollMain.java
@@ -0,0 +1,16 @@
+package UC2_DemonstrateFileOperations;
+
+public class EmployeePayrollMain {
+	
+	public static void main(String[] args) {
+		System.out.println("..WELCOME TO EMPLOYEE PAYROLL SYSTEM..");
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+
+		employeePayrollService.readEmployeeDataFromConsole();
+		employeePayrollService.writeEmployeeDataInConsole();
+	}
+
+}
+
+
+

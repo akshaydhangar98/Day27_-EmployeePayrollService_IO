f3ac6b17764d7bacc58898059573ed53215f1884
akshay dhangar <akshaygdhangar14@gmail.com>
2022-07-11 13:10:17 +0530

    UC4_Create_Employee_Payroll

---
+++ UC_4/UC4_StorePayroll/EmployeePayrollMain.java
@@ -0,0 +1,14 @@
+package UC4_StorePayroll;
+
+public class EmployeePayrollMain {
+	public static void main(String[] args) {
+		System.out.println("..WELCOME TO EMPLOYEE PAYROLL SYSTEM..");
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+
+		employeePayrollService.readEmployeeDataFromConsole();
+		employeePayrollService.writeEmployeeDataInConsole();
+	}
+
+}
+
+
